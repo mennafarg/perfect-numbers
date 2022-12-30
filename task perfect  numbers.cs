9687e51fae5e64b48d40e8bc65b1f24ae5b4c582
mennafarg <116466807+mennafarg@users.noreply.github.com>
2022-12-30 21:35:16 +0200

    Add files via upload

---
+++ task perfect  numbers.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+static bool IsPerfect(int number)
+{
+    int sum = 0;
+    for (int i =1; i< number; i++)
+    {
+        if (number % i == 0) sum = sum + i;
+
+
+    }
+    if (sum == number) return true;
+    return false;
+}
+
+Console.WriteLine("hello,please enrer first number: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.WriteLine("hello,please enrer second number: ");
+int second = int.Parse(Console.ReadLine());
+for (int i = first; i <= second; i++)
+{
+    if (IsPerfect(i)) Console.WriteLine("{0}" ,i);
+}
+
+
